4193c0c66f3c45c8154e98cec22eff2cd9f8e702
Matej Capkovic <mcapkovic@gmail.com>
2021-03-04 07:30:28 +0100

    Add env variables

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 const config = {
-
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 }
 
 if (!firebase.apps.length) {
@@ -25,7 +31,9 @@ const firestore = firebase.firestore();
 function App() {
 
   const [user] = useAuthState(auth);
+  console.log(process.env)
 
+console.log(process.env.REACT_APP_NOT_SECRET_CODE)
   return (
     <div className="App">
 
