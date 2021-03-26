c92ed07fa664c6694ee259ac85a25e66c1c94ac7
Matej Capkovic <mcapkovic@gmail.com>
2021-03-26 18:33:38 +0100

    Change name

--- src/common/ContentPortal.js
+++ src/common/ContentPortal.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { getClassNames } from "../utils/common";
-import Button from "./Button";
 
 function ContentPortal(props) {
   const { className, portalTo = "", ...otherProps } = props;
 
-  const headerStart = React.useRef(document.querySelector(portalTo));
+  const element = React.useRef(document.querySelector(portalTo));
   return ReactDOM.createPortal(
     <div
       {...otherProps}
       className={getClassNames(className, "content-portal")}
     />,
-    headerStart.current
+    element.current
   );
 }
 
