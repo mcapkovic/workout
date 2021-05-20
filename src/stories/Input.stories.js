2e8cd35c32f6dca143737e4dc5c99d76a9bbab50
Matej Capkovic <mcapkovic@gmail.com>
2021-05-20 21:36:27 +0200

    Add storybook

---
+++ src/stories/Input.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { TextBox } from "../common";
+
+export default {
+  title: "Inputs/TextBox",
+  component: TextBox,
+};
+
+const Template = (args) => <TextBox {...args} />;
+
+export const Primary = Template.bind({});
+
